10205acf90b9a56e8b6659c8e290880a32523ee9
Maksim Talochka <maksim_talochka@epam.com>
2015-04-17 15:00:30 +0300

    add CRUD operations

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,12 +1,45 @@
 module.exports = function (sequelize) {
     var News = require('../models/news')(sequelize);
     return {
-        create: function (req, res) {
-
-        },
-        get: function (req, res) {
-            News.findAll().success(function (news) {
+        getAllNews: function (req, res) {
+            News.findAll({attributes: ['id', 'title', 'shortDescription', 'createdAt', 'updatedAt']}).then(function (news) {
                 res.send(news);
+            });
+        },
+        getNews: function (req, res) {
+            News.find(req.params.id).then(function (news) {
+                res.send(news);
+            });
+        },
+        changeNews: function (req, res) {
+            News.find(req.params.id).then(function (news) {
+                if(news) {
+                    news.updateAttributes({
+                        title: req.body.title,
+                        shortDescription: req.body.shortDescription,
+                        body: req.body.body
+                    }).then(function() {
+                        res.send(news);
+                    })
+                }
+            });
+        },
+        addNews: function (req, res) {
+            News.create({
+                title: req.body.title,
+                shortDescription: req.body.shortDescription,
+                body: req.body.body
+            }).then(function (news) {
+                res.send(news);
+            })
+        },
+        deleteNews: function (req, res) {
+            News.find(req.params.id).then(function (news) {
+                if(news) {
+                    news.destroy().then(function() {
+                        res.end();
+                    })
+                }
             });
         }
     };
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,17 @@
 module.exports = function(app, controller) {
 
     app.get('/', function (req, res) {
-        res.sendfile('index.html');
+        res.sendfile('../public/index.html');
     });
 
-    app.get('/api/get', controller.get);
+    app.get('/api/news', controller.getAllNews);
+
+    app.get('/api/news/:id', controller.getNews);
+
+    app.post('/api/news/:id', controller.changeNews);
+
+    app.put('/api/news', controller.addNews);
+
+    app.delete('/api/news/:id', controller.deleteNews);
 
 };
